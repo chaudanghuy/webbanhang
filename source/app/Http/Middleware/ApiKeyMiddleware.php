7d4d2019f26a8d2f9c76216706662c0ef4a6b203
Huy Chau <huy.chau@besmartee.com>
2024-06-12 10:46:25 +0700

    remove auth

--- source/app/Http/Middleware/ApiKeyMiddleware.php
+++ source/app/Http/Middleware/ApiKeyMiddleware.php
@@ -17,11 +17,11 @@ class ApiKeyMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->hasHeader('x-api-key')) {
-            if ($request->header('x-api-key') == env('MIX_API_KEY')) {
-                return $next($request);
-            }
-        }
-        return response()->json(trans('all.message.invalid_api_key'), 400);
+        // if ($request->hasHeader('x-api-key')) {
+        //     if ($request->header('x-api-key') == env('MIX_API_KEY')) {
+        //         return $next($request);
+        //     }
+        // }
+        // return response()->json(trans('all.message.invalid_api_key'), 400);
     }
 }
